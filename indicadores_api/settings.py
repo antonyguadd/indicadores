405f95ce9ba4ae3b352555a14e5feb7b423c2ac2
Antonio Guadalupe Flores Torres <antonyguad@gmail.com>
2024-10-20 21:55:42 -0600

    errores

--- indicadores_api/settings.py
+++ indicadores_api/settings.py
@@ -215,5 +215,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-CORS_ALLOWED_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "https://indicadores-6nfbvvf2j-antonyguads-projects.vercel.app",
+    "http://localhost:3000",
+]
